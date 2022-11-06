1fa08134fcbfd5282038b21b5fd28eb1f5e6cca3
Kristian Ivanov <kriskopk@abv.bg>
2022-11-06 23:25:10 +0200

    refactored some css files,added footer component and mail list

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Featured from "../../components/Featured/Featured.js";
 import FeaturedProperties from "../../components/FeaturedProperties/FeaturedProperties.js";
+import Footer from "../../components/Footer/Footer.js";
 import { Header } from "../../components/Header/Header.js";
+import MailList from "../../components/MailList/MailList.js";
 import { Navbar } from "../../components/Navbar/Navbar.js";
 import PropertyList from "../../components/PropertyList/PropertyList.js";
 import "./Home.scss";
@@ -16,6 +18,8 @@ export const Home = () => {
       <PropertyList />
       <h1 className="homeTitle">Homes guests love</h1>
       <FeaturedProperties />
+      <MailList />
+      <Footer />
     </div>
   </div>;
 };
