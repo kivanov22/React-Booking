b199099b6af25c0b19f203f43bc6f96155e5921a
Kristian Ivanov <kriskopk@abv.bg>
2022-11-11 23:07:16 +0200

    User route logic created.And a function for verifying jwt token.And another for verifying user.

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,18 @@
 import express from 'express'
 import { deleteUser, getAllUsers, getUser, updateUser } from '../controllers/userController.js';
+import { verifyToken } from '../utils/verifyToken.js';
+import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
+
+router.get("/checkauthentication",verifyToken,(req,res,next)=>{
+    res.send("hello user, you are logged in")
+});
+
+router.get("/checkuser/:id",verifyUser,(req,res,next)=>{
+    res.send("hello user, you are logged in and you can delete your account")
+});
+
 
 //UPDATE
 router.put("/:id",updateUser)
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,12 @@
+import { createError } from "./error.js"
+import { verifyToken } from "./verifyToken.js"
+
+export const verifyUser = (req,res,next)=>{
+    verifyToken(req,res,next,()=>{
+        if(req.user.id === req.params.id || req.user.isAdmin){
+        next()
+        }else {
+            if(err) return next(createError(403,"You are not authorized!"));
+        }
+    })
+}
