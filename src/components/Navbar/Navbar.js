e37c0e5c0c851139c764e8301e14b9222ba7a976
Kristian Ivanov <kriskopk@abv.bg>
2022-11-22 12:36:52 +0200

    Changed logo navigating to homepage now.Replaced static data with data from db in FeaturedProperties component.Added retrieving hotels with query and by min/max price with limit of display hotels.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import "./Navbar.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 export const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className="navContainer">
-            <span className="logo">IvanovBooking</span>
-            <div className="navItems">
-                <button className="navButton">Login</button>
-                <button className="navButton">Register</button>
-            </div>
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{color:"inherit",textDecoration:"none"}}>
+          <span className="logo">IvanovBooking</span>
+        </Link>
+        <div className="navItems">
+          <button className="navButton">Login</button>
+          <button className="navButton">Register</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
