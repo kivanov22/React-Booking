02ddec021f4a83ebf1092ba91504d36ed296ad52
Kristian Ivanov <kriskopk@abv.bg>
2022-11-24 19:45:27 +0200

    Created auth context and wrapped the app. Added Login page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/Home.js";
 import { Hotel } from "./pages/hotel/Hotel.js";
 import { List } from "./pages/list/List.js";
+import Login from "./pages/login/Login.js";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/hotels" element={<List />}/>
         <Route path="/hotels/:id" element={<Hotel />}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </BrowserRouter>
   );
